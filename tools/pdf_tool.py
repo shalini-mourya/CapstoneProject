afbe4af91f63b443607b12ead890f29e39f6c7ce
Shalini <smou25@gmail.com>
2025-11-26 11:03:13 +0530

    added name str

--- tools/pdf_tool.py
+++ tools/pdf_tool.py
@@ -5,7 +5,7 @@ from utils.pdf_utils import generate_pdf
 
 @dataclass
 class PDFTool:
-    name: str = "generate_pdf"
+    name: str = "pdf_generate"
 
     def run(self, prompt: str, response: str, prefs: Dict[str, Any] = None) -> Dict[str, Any]:
         pdf_result = generate_pdf(prompt, response, prefs)
